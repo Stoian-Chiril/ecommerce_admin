f5e8dcbc099ff9bbe450ac06f1aee6e011dc9c0b
Stoian-Chiril <chiril.stoian@isa.utm.md>
2024-10-18 09:04:53 +0300

    Update route.ts

--- app/api/[storeid]/checkout/route.ts
+++ app/api/[storeid]/checkout/route.ts
@@ -71,14 +71,12 @@ export async function POST(
         phone_number_collection: {
             enabled: true
         },
-        success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-        cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
+        success_url: `${process.env.FRONTEND_STORE_URL}/stores/${params.storeid}?success=1`,
+        cancel_url: `${process.env.FRONTEND_STORE_URL}/stores/${params.storeid}?canceled=1`,
         metadata: {
             orderId: order.id
         }
     });
 
-    return NextResponse.json({url:session.url},{
-        headers: corsHeaders
-    });
+    return NextResponse.json({url:session.url},{headers: corsHeaders});
 }
